06aec10bddea0386b96b2a6c87ba84b750939933
Hyeon Jin <choo0618@naver.com>
2019-04-02 23:12:27 +0900

    algorithm

--- algoritm/day31.py
+++ algoritm/day31.py
@@ -1,38 +1,37 @@
 import sys
 sys.stdin=open('input.txt','r')
 
-# 상원이의 생일파티
-def Find(x):
-    if x==P[x]:return x
-    else:return Find(P[x])
-def Union(x,y):
-    P[Find(y)]=Find(x)
+# # 상원이의 생일파티
+def BFS(y,x):
+    global cnt
+    Q.append([y,x])
+    while Q:
+        tmp=Q.pop(0)
+        hy=tmp[0]
+        hx=tmp[1]
+        here=V[hy][hx]
+        if here==1 or here==2:
+            cnt+=1
+        for i in range(2,len(M[1])):
+            if M[hx][i] and not V[hx][i]:
+                Q.append([hx,i])
+                V[hx][i]=here+1
 T=int(input())
 for n in range(T):
     L=[int(x)for x in input().split()]
     A=[[int(x)for x in input().split()]for y in range(L[1])]
-    P=[0]+[0]*(L[0])
-    for j in range(L[0]+1):
-        P[j]=j
-    for i in range(L[1]):
-        Union(A[i][0],A[i][1])
-    print(P)
-    cnt=[]
-    for c in range(len(P)):
-        if Find(c):cnt.append(Find(c))
-    print(cnt)
-    print('#%d %d'%((n+1),len(cnt)-1))
-
+    M=[[0]*(L[0]+1)for _ in range(L[0]+1)]
+    V=[[0]*(L[0]+1)for _ in range(L[0]+1)]
+    for y in range(L[1]):
+        M[A[y][0]][A[y][1]]=1
+        M[A[y][1]][A[y][0]]=1
+    Q=[]
+    cnt=0
+    for i in range(len(M[1])):
+        if M[1][i]:BFS(1,i)
+    print('#%d %d'%(n+1,cnt))
 
 # # 격자판의 숫자 이어 붙이기
-# def DFS(x,c,r):
-#     if c==7:
-#         if not r in R:
-#             R.append(r)
-#         return
-#     if x==16:return
-#     DFS(x+1,c+1,r+str(L[x]))
-#     DFS(x+1,c,r)
 # dy=[0,1,0,-1]
 # dx=[1,0,-1,0]
 # def IsSafe(y,x):
