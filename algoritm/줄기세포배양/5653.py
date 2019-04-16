832e76f62afcd2aaac96ccd222c633a5e16b0f75
Hyeon Jin <choo0618@naver.com>
2019-04-16 23:56:11 +0900

    algorithm

--- algoritm/줄기세포배양/5653.py
+++ algoritm/줄기세포배양/5653.py
@@ -4,38 +4,36 @@ sys.stdin = open('5653.txt', 'r')
 dy=[-1,0,1,0]
 dx=[0,1,0,-1]
 def V(y,x):
-    if not y:y+=1
-    tmp=0
-    while tmp!=len(A):
-        if A[tmp][0]==1:x+=1;break
-        tmp+=1
+    global Q
     for dir in range(4):
         nY=y+dy[dir]
         nX=x+dx[dir]
         if not M[nY][nX]:
-            M[nY][nX]=M[y][x]
-    M[y][x]='d'
-
-L=[int(x)for x in input().split()]
-print(L)
-A=[[int(x)for x in input().split()]for y in range(L[0])]
-print(A)
-T=L[2]
-M=[[1,1],[0,2]]
-print(M)
-while T:
-    T-=1
-    if 1 in A[0]:
-        M.insert(0,[0]*len(M[0]))
-    if 1 in A[-1]:
-        M.append([0]*len(M[0]))
-    for k in range(len(M)):
-        M[k].insert(0,0)
-        M[k].append(0)
-    for j in range(len(A[0])):
-        for i in range(len(A)):
-            if A[j][i]==1:
-                V(j,i)
-                # A[j][i]='d'
-            elif A[j][i]:
-                A[j][i]-=1
+            A[nY][nX]=A[y][x]
+            Q.append([nY,nX,A[y][x]])
+T=int(input())
+for n in range(T):
+    L=[int(x)for x in input().split()]
+    A=[[0]*(300+L[1])for _ in range(150)]+[[0]*150+[int(x)*2for x in input().split()]+[0]*150for y in range(L[0])]+[[0]*(300+L[1])for _ in range(150)]
+    M=[[0]*len(A[0])for m in range(len(A))]
+    for my in range(len(A)):
+        for mx in range(len(A[0])):
+            if A[my][mx]:M[my][mx]=A[my][mx]
+    Q=[]
+    while L[2]:
+        L[2]-=1
+        for j in range(len(A)):
+            for i in range(len(A[0])):
+                if M[j][i] and type(M[j][i])==int:
+                    if M[j][i]==A[j][i]//2:V(j,i)
+                    if M[j][i]==1:M[j][i]='d'
+                    else:M[j][i]-=1
+        while Q:
+            tmp=Q.pop(0)
+            if tmp[2]>M[tmp[0]][tmp[1]]:
+                M[tmp[0]][tmp[1]]=tmp[2]
+    cnt=0
+    for ry in range(len(A)):
+        for rx in range(len(A[0])):
+            if M[ry][rx] and not M[ry][rx]=='d':cnt+=1
+    print('#%d %d'%(n+1,cnt))
