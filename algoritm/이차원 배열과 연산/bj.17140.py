85966f5ca719f74c5eb5f18cd6544129ba23c032
Hyeon Jin <choo0618@naver.com>
2019-05-07 17:42:23 +0900

    190507 | Vue Basic

--- algoritm/이차원 배열과 연산/bj.17140.py
+++ algoritm/이차원 배열과 연산/bj.17140.py
@@ -14,31 +14,33 @@ def R():
                 l.append([k,M[k]])
         l.sort(key=lambda t:t[1])
         for z in l:
-            ll+=z
+            if len(ll)<=100:ll+=z
         if len(ll)>r:r=len(ll)
         A[i]=ll
     for m in range(len(A)):
         if len(A[m])!=r:
             A[m]+=[0]*(r-len(A[m]))
 def C():
+    global A
+    A=list(map(list,zip(*A[::-1])))
     R()
+    A=list(map(list, zip(*A[::1])))
+def check():
+    if len(A)>L[0]-1 and len(A[0])>L[1]-1:
+        if A[L[0]-1][L[1]-1]==L[2]:return True
+    return False
 L=[int(x)for x in input().split()]
 A=[[int(x)for x in input().split()]for y in range(3)]
-
-print(L)
-print(A)
 Result=0
 while True:
-    # if A[L[0]-1][L[1]-1]==L[2]:break
-    if len(A)<=len(A[0]):R()
-    Result+=1
-    # if A[L[0]-1][L[1]-1]==L[2]:break
-    A=list(map(list, zip(*A[::-1])))
-    if len(A)>len(A[0]):
+    if Result>100:Result=-1;break
+    if check():break
+    if len(A)>=len(A[0]):
+        R()
+        Result+=1
+    if check():break
+    if len(A)<len(A[0]):
         C()
-        for i in range(len(A)):
-            for j in range(i+1,len(A[0]),1):
-                A[i][j],A[j][i]=A[j][i],A[i][j]
-    Result+=1
-    # if A[L[0]-1][L[1]-1]==L[2]: break
+        Result+=1
+    if check():break
 print(Result)
