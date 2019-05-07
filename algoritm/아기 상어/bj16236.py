881a050a5e25554d40372fafc756cea0ec3a21f6
Hyeon Jin <choo0618@naver.com>
2019-05-07 23:45:46 +0900

    algorithm

---
+++ algoritm/아기 상어/bj16236.py
@@ -0,0 +1,45 @@
+import sys
+sys.stdin = open('bj16236.txt','r')
+
+dy=[-1,0,0,1]
+dx=[0,-1,1,0]
+def IS(y,x,s):
+    if -1<y<N and -1<x<N and A[y][x]<=s:return True
+    return False
+def BFS(y,x,s,c,r):
+    global R
+    while True:
+        M=[[0]*N for _ in range(N)]
+        if c==0:s+=1;c=s
+        Q=[]
+        Q.append([y,x,r])
+        M[y][x]=9
+        t=1
+        d=0
+        while Q:
+            tmp=Q.pop(0)
+            hY=tmp[0]
+            hX=tmp[1]
+            hr=tmp[2]
+            if 0<A[hY][hX]<s:
+                y=hY
+                x=hX
+                c-=1
+                A[y][x]=0
+                R+=hr
+                t=0
+                break
+            for dir in range(4):
+                nY=hY+dy[dir]
+                nX=hX+dx[dir]
+                if IS(nY,nX,s) and not M[nY][nX]:Q.append([nY,nX,hr+1]);M[nY][nX]=9
+        if t:break
+N=int(input())
+A=[[int(x)for x in input().split()]for y in range(N)]
+R=0
+for i in range(N):
+    for j in range(N):
+        if A[i][j]==9:
+            A[i][j]=0
+            BFS(i,j,2,2,0)
+print(R)
