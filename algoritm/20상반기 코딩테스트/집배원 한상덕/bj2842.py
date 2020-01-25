9bac45ee11da2fa8c41be0575e53492426cc1e3a
Hyeon Jin <choo0618@naver.com>
2020-01-25 22:59:13 +0900

    200125 | algorithm

--- algoritm/20상반기 코딩테스트/집배원 한상덕/bj2842.py
+++ algoritm/20상반기 코딩테스트/집배원 한상덕/bj2842.py
@@ -1,40 +1,41 @@
 import sys
 sys.stdin = open('bj2842.txt','r')
 
-dx=[1,1,1,0,0,-1,-1,-1]
+dx=[1,1,1,0,-1,-1,-1,0]
 dy=[-1,0,1,1,1,0,-1,-1]
 def IS(y,x):
     return -1<y<N and -1<x<N
-def BFS(y,x,t):
-    Que=[(y,x)]
-    Map[y][x]=t
+N=int(input())
+A=[list(map(str,input()))for y in range(N)]
+H,L=[],[]
+for h in range(N):
+    I=[int(x)for x in input().split()]
+    L+=I
+    H.append(I)
+L.sort()
+Min,Max,P,K,R=0,0,0,0,987654321
+for i in range(N):
+    for j in range(N):
+        if A[i][j]=='P':P=(i,j)
+        elif A[i][j]=='K':K+=1
+for i in range(N*N):
+    if L[i]==H[P[0]][P[1]]:Max=i;tmp=i;break
+while Min<=Max and Min!=tmp+1:
+    Que,h=[P],0
+    Map=[[0]*N for _ in range(N)]
     while Que:
         Q=[]
         for q in Que:
             for d in range(8):
                 Y,X=q[0]+dy[d],q[1]+dx[d]
-                if IS(Y,X) and not Map[Y][X] and Min<=H[Y][X]<=Max:
-                    Map[Y][X]=t
+                if IS(Y,X) and L[Min]<=H[Y][X]<=L[Max] and not Map[Y][X]:
+                    if A[Y][X]=='K':h+=1
+                    Map[Y][X]=1
                     Q.append((Y,X))
         Que=Q
-N=int(input())
-A=[list(map(str,input()))for y in range(N)]
-H=[[int(x)for x in input().split()]for y in range(N)]
-Que,R,Sort=[],987654321,[]
-# Min,Max=987654321,0
-Min,Max=0,1
-for i in range(N):
-    for j in range(N):
-        Sort.append(H[i][j])
-        if A[i][j]!='.':
-            Min=min(Min,H[i][j])
-            Max=max(Max,H[i][j])
-            Que.append((i,j))
-Sort.sort()
-print(Sort)
-while True:
-    Map,tmp=[[0]*N for _ in range(N)],1
-    for q in Que:
-        if not Map[q[0]][q[1]] and Sort[Min]<=A[q[0]][q[1]]<=Sort[Max]:BFS(q[0],q[1],tmp);tmp+=1
-
-print(Max-Min)
+    if h==K:
+        R=min(R,L[Max]-L[Min])
+        Min+=1
+    elif Max+1<N*N:Max+=1
+    else:break
+print(R)
