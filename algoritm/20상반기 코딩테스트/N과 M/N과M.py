0dccb0013e677ac66fce84873b3910a478466ef9
Hyeon Jin <choo0618@naver.com>
2020-01-28 00:06:31 +0900

    200128 | algorithm

--- algoritm/20상반기 코딩테스트/N과 M/N과M.py
+++ algoritm/20상반기 코딩테스트/N과 M/N과M.py
@@ -1,7 +1,7 @@
 import sys
 sys.stdin=open('input.txt','r')
 
-# 1
+# # 1
 # def DFS(cnt,List):
 #     if cnt==M:
 #         for p in List:
@@ -18,7 +18,7 @@ sys.stdin=open('input.txt','r')
 # Map=[0]*N
 # DFS(0,[])
 
-# 2
+# # 2
 # def DFS(idx,cnt,List):
 #     if cnt==M:
 #         for p in List:
@@ -36,7 +36,7 @@ sys.stdin=open('input.txt','r')
 # Map=[0]*N
 # DFS(0,0,[])
 
-# 3
+# # 3
 # def DFS(cnt,List):
 #     if cnt==M:
 #         print(' '.join(map(str, List)))
@@ -48,7 +48,7 @@ sys.stdin=open('input.txt','r')
 # N,M=map(int,input().split())
 # DFS(0,[])
 
-# 4
+# # 4
 # def DFS(idx,cnt,List):
 #     if cnt==M:
 #         print(' '.join(map(str, List)))
@@ -59,3 +59,137 @@ sys.stdin=open('input.txt','r')
 #         List.pop()
 # N,M=map(int,input().split())
 # DFS(0,0,[])
+
+# # 5
+# def DFS(idx,cnt,List):
+#     if cnt==M:
+#         print(' '.join(map(str,List)))
+#         return
+#     for i in range(N):
+#         if Map[i]:continue
+#         Map[i]=1
+#         List.append(L[i])
+#         DFS(i+1,cnt+1,List)
+#         Map[i]=0
+#         List.pop()
+#
+# N,M=map(int,input().split())
+# L=[int(x)for x in input().split()]
+# L.sort()
+# Map=[0]*N
+# DFS(0,0,[])
+
+# # 6
+# def DFS(idx,cnt,List):
+#     if cnt==M:
+#         print(' '.join(map(str,List)))
+#     for i in range(idx,N):
+#         List.append(L[i])
+#         DFS(i+1,cnt+1,List)
+#         List.pop()
+#
+# N,M=map(int,input().split())
+# L=[int(x)for x in input().split()]
+# L.sort()
+# DFS(0,0,[])
+
+# # 7
+# def DFS(cnt,List):
+#     if cnt==M:
+#         print(' '.join(map(str,List)))
+#         return
+#     for i in range(N):
+#         List.append(L[i])
+#         DFS(cnt+1,List)
+#         List.pop()
+# N,M=map(int,input().split())
+# L=[int(x)for x in input().split()]
+# L.sort()
+# DFS(0,[])
+
+# # 8
+# def DFS(idx,cnt,List):
+#     if cnt==M:
+#         print(' '.join(map(str,List)))
+#         return
+#     for i in range(idx,N):
+#         List.append(L[i])
+#         DFS(i,cnt+1,List)
+#         List.pop()
+# N,M=map(int,input().split())
+# L=[int(x)for x in input().split()]
+# L.sort()
+# DFS(0,0,[])
+
+# # 9
+# def DFS(cnt,List):
+#     if cnt==M:
+#         print(' '.join(map(str,List)))
+#         return
+#     Map=[0]*(max(L)+1)
+#     for i in range(N):
+#         if Map[L[i]]or Map1[i]:continue
+#         List.append(L[i])
+#         Map[L[i]]=1
+#         Map1[i]=1
+#         DFS(cnt+1,List)
+#         List.pop()
+#         Map1[i]=0
+# N,M=map(int,input().split())
+# L=[int(x)for x in input().split()]
+# L.sort()
+# Map1=[0]*N
+# DFS(0,[])
+
+# # 10
+# def DFS(idx,cnt,List):
+#     if cnt==M:
+#         print(' '.join(map(str,List)))
+#         return
+#     Map=[0]*(max(L)+1)
+#     for i in range(idx,N):
+#         if Map[L[i]]:continue
+#         List.append(L[i])
+#         Map[L[i]]=1
+#         DFS(i+1,cnt+1,List)
+#         List.pop()
+# N,M=map(int,input().split())
+# L=[int(x)for x in input().split()]
+# L.sort()
+# DFS(0,0,[])
+
+# # 11
+# def DFS(cnt,List):
+#     if cnt==M:
+#         print(' '.join(map(str,List)))
+#         return
+#     Map=[0]*(max(L)+1)
+#     for i in range(N):
+#         if Map[L[i]]:continue
+#         List.append(L[i])
+#         Map[L[i]]=1
+#         DFS(cnt+1,List)
+#         List.pop()
+# N,M=map(int,input().split())
+# L=[int(x)for x in input().split()]
+# L.sort()
+# DFS(0,[])
+
+# # 12
+# def DFS(idx,cnt,List):
+#     if cnt==M:
+#         print(' '.join(map(str,List)))
+#         return
+#     Map=[0]*(max(L)+1)
+#     for i in range(idx,N):
+#         if Map[L[i]]:continue
+#         List.append(L[i])
+#         Map[L[i]]=1
+#         DFS(i,cnt+1,List)
+#         List.pop()
+# N,M=map(int,input().split())
+# L=[int(x)for x in input().split()]
+# L.sort()
+# DFS(0,0,[])
+
+
---
+++ algoritm/20상반기 코딩테스트/파이프 옮기기 1/bj17070.py
@@ -0,0 +1,79 @@
+import sys
+sys.stdin=open('bj17070.txt','r')
+
+# dx=[1,0,1]
+# dy=[0,1,1]
+# def IS(y,x):
+#     return -1<y<N and -1<x<N
+# def DFS(y,x,d):
+#     R=0
+#     if (x,y)==(N-1,N-1):return 1
+#     for dir in range(3):
+#         if d+dir==1:continue
+#         Y,X=y+dy[dir],x+dx[dir]
+#         if not IS(Y,X):continue
+#         if dir==2 and (Arr[x][y+1] or Arr[x+1][y]):continue
+#         R+=DFS(Y,X,dir)
+#     return R
+# N=int(input())
+# Arr=[[int(x)for x in input().split()]for y in range(N)]
+# print(DFS(0, 1, 0))
+
+# from collections import deque
+# dx=[1,0,1]
+# dy=[0,1,1]
+# Dic={0:1,1:0,2:-1}
+# def IS(y,x):
+#     return -1<y<N and -1<x<N
+# def Check(y,x):
+#     for d in range(2):
+#         Y,X=y+dy[d],x+dx[d]
+#         if not IS(Y,X) or Arr[Y][X]:return False
+#     return True
+# N=int(input())
+# Arr=[[int(x)for x in input().split()]for y in range(N)]
+# Map=[[[0,0,0]for _ in range(N)]for __ in range(N)]
+# Que=deque([(0,1,0)])
+# Map[0][1][0]=1
+# R=0
+# while Que:
+#     y,x,dir=Que.popleft()
+#     if (y,x)==(N-1,N-1):R+=1;continue
+#     for d in range(3):
+#         if Dic[dir]==d or (d==2 and not Check(y,x)):continue
+#         Y,X=y+dy[d],x+dx[d]
+#         if IS(Y,X) and not Arr[Y][X]:
+#             if d==2:Map[Y][X][2]=
+#             else:
+#                 Map[Y][X][d]=1
+#                 Que.append((Y,X,d))
+# print(Map[N-1][N-1])
+
+
+N=int(input())
+Arr=[[int(x)for x in input().split()]for y in range(N)]
+Map=[[[0,0,0]for _ in range(N)]for __ in range(N)]
+Map[0][1][0]=1
+for x in range(2,N):
+    if not Arr[0][x]:Map[0][x][0]=Map[0][x-1][0]
+for y in range(N):
+    for x in range(2,N):
+        if Arr[y][x]==Arr[y-1][x]==Arr[y][x-1]==0:
+            Map[y][x][2]=Map[y-1][x-1][0]+Map[y-1][x-1][1]+Map[y-1][x-1][2]
+        if not Arr[y][x]:
+            Map[y][x][0]=Map[y][x-1][2]+Map[y][x-1][0]
+            Map[y][x][1]=Map[y-1][x][2]+Map[y-1][x][1]
+print(sum(Map[-1][-1]))
+
+def pipe(x,y,d):
+    global ans
+    if x==N-1==y:ans+=1
+    if (d==0 or d==1) and y+1<N and A[x][y+1]==0:pipe(x,y+1,0)
+    if x+1<N and y+1<N and A[x][y+1]==A[x+1][y]==A[x+1][y+1]==0:pipe(x+1,y+1,1)
+    if (d==1 or d==2) and x+1<N and A[x+1][y]==0:pipe(x+1,y,2)
+N=int(input())
+A=[[int(x)for x in input().split()] for _ in range(N)]
+R=0
+pipe(0,1,0)
+print(R)
+
