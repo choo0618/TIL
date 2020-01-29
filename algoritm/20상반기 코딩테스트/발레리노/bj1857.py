3a599365f18ac3ad633a9df3172ca288e530603e
Hyeon Jin <choo0618@naver.com>
2020-01-30 01:30:37 +0900

    200130 | algorithm

--- algoritm/20상반기 코딩테스트/발레리노/bj1857.py
+++ algoritm/20상반기 코딩테스트/발레리노/bj1857.py
@@ -1,6 +1,7 @@
 import sys
 sys.stdin = open('bj1857.txt','r')
 
+import heapq
 dx=[1,2,2,1,-1,-2,-2,-1]
 dy=[-2,-1,1,2,2,1,-1,-2]
 def IS(y,x):
@@ -9,45 +10,42 @@ M,N=map(int,input().split())
 A=[[int(x)for x in input().split()]for y in range(M)]
 for i in range(M):
     for j in range(N):
-        if A[i][j]==3:S=(i,j,0)
+        if A[i][j]==3:S=(0,i,j)
         elif A[i][j]==4:E=(i,j)
 Que=[S]
-Map=[[[999]*8 for _ in range(N)]for __ in range(M)]
+Map=[[[0]*8 for _ in range(N)]for __ in range(M)]
 Map[Que[0][0]][Que[0][1]]=[0]*8
 Min=987654321
-while Que:
-    Q=[]
-    for q in Que:
-        if (q[0],q[1])==E:
-            Min=min(q[2],Min)
-            continue
-        for d in range(8):
-            nY,nX,nC=q[0]+dy[d],q[1]+dx[d],q[2]
-            if IS(nY,nX) and A[nY][nX]!=2:
-                if not A[nY][nX]:nC+=1
-                if Map[nY][nX][d]>nC:
-                    Map[nY][nX][d]=nC
-                    Q.append((nY,nX,nC))
-    Que=Q
-print(Min)
-Que=[S]
-Map=[[[999]*8 for _ in range(N)]for __ in range(M)]
-Map[Que[0][0]][Que[0][1]]=[0]*8
 R=0
 while Que:
-    Q=[]
-    for q in Que:
-        if q[2]==Min:
-            if (q[0],q[1])==E:R+=1
-            continue
-        for d in range(8):
-            nY,nX,nC=q[0]+dy[d],q[1]+dx[d],q[2]
-            if IS(nY,nX) and A[nY][nX]!=2:
-                if (nC==Min and (nY,nX)!=E) or Map[nY][nX][d]<nC:continue
-                if not A[nY][nX]: nC += 1
-                Map[nY][nX][d]=nC
-                Q.append((nY,nX,nC))
-    Que=Q
+    C,Y,X=heapq.heappop(Que)
+    if C>Min+1:continue
+    if A[Y][X]==4:Min=min(Min,C);R+=1;continue
+    for d in range(8):
+        nY,nX,nC=Y+dy[d],X+dx[d],C
+        if IS(nY,nX) and A[nY][nX]!=2 and not Map[nY][nX][d]:
+            if not A[nY][nX]:nC+=1
+            Map[nY][nX][d]=1
+            heapq.heappush(Que,(nC,nY,nX))
 print(Min)
 print(R)
+
+# Que=[S]
+# Map=[[[0]*8 for _ in range(N)]for __ in range(M)]
+# Map[Que[0][0]][Que[0][1]]=[1]*8
+# R=0
+# while Que:
+#     C,Y,X=heapq.heappop(Que)
+#     for d in range(8):
+#         nY,nX,nC=Y+dy[d],X+dx[d],C
+#         if IS(nY,nX) and A[nY][nX]!=2:
+#             if q[2]==Min and A[q[0]][q[1]]==4:R+=1;print(R);break
+#             if not A[nY][nX]:nC+=1
+#             Q.append((nY,nX,nC))
+#     Que=Q
+# print(Min)
+# print(R)
+#
+
+
 
