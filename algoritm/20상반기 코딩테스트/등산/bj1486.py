47954196e4883b0d3af1f5bb3872f3d8700221ce
Hyeon Jin <choo0618@naver.com>
2020-02-03 01:18:24 +0900

    200203 | algorithm

--- algoritm/20상반기 코딩테스트/등산/bj1486.py
+++ algoritm/20상반기 코딩테스트/등산/bj1486.py
@@ -1,25 +1,30 @@
 import sys
 sys.stdin = open('bj1486.txt','r')
 
-print(ord('A')-65)
-print(ord('Z')-65)
-print(ord('a')-71)
-print(ord('z')-71)
-
 from collections import deque
 dx=[1,0,-1,0]
-dy=[0,-1,0,-1]
+dy=[0,1,0,-1]
 def IS(y,x):
     return -1<y<N and -1<x<M
 def BFS(q,Mid):
-    Map=[[[-1]*M for _ in range(N)]for __ in range(N)]
+    if A[0][0]>=Mid:return True
+    Map=[[[-1]*M for _ in range(N)]for __ in range(2)]
     Map[0][0][0]=0
     Que=deque([q])
     while Que:
         y,x,t,tmp=Que.popleft()
+        if Map[1][0][0]!=-1:return True
         for d in range(4):
-
-
+            Y,X,ntmp=y+dy[d],x+dx[d],tmp
+            if IS(Y,X):
+                if abs(A[Y][X]-A[y][x])>T:continue
+                if A[Y][X]>=Mid:ntmp=1
+                if A[y][x]>=A[Y][X]:nT=t+1
+                else:nT=t+(A[Y][X]-A[y][x])**2
+                if nT<=D and (Map[ntmp][Y][X]==-1 or Map[ntmp][Y][X]>nT):
+                    Map[ntmp][Y][X]=nT
+                    Que.append((Y,X,nT,ntmp))
+    return False
 N,M,T,D=map(int,input().split())
 A=[list(map(str,input()))for _ in range(N)]
 for i in range(N):
@@ -28,8 +33,9 @@ for i in range(N):
         if k>=97:k-=71
         else:k-=65
         A[i][j]=k
-l,r=0,51
+l,r=A[0][0],51
 while l<=r:
     Mid=(l+r)//2
     if BFS((0,0,0,0),Mid):l=Mid+1
-    else:R=Mid-1
+    else:r=Mid-1
+print(r)
