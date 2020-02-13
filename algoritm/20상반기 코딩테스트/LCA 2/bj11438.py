1129e7e5355d61bba8e1c1052dce7468a2a8d359
Hyeon Jin <choo0618@naver.com>
2020-02-14 01:11:13 +0900

    200214 | algorithm

--- algoritm/20상반기 코딩테스트/LCA 2/bj11438.py
+++ algoritm/20상반기 코딩테스트/LCA 2/bj11438.py
@@ -1,15 +1,35 @@
 import sys
 sys.stdin = open('bj11438.txt','r')
 
-def find(x):
-    if P[x]!=x:P[x]=find(P[x])
-    return P[x]
-def union(a,b):
-    P[find(a)]=find(b)
+input=sys.stdin.readline
+from collections import deque
+def LCA(x,y):
+    if D[x]>D[y]:x,y=y,x
+    for i in range(20,-1,-1):
+        if D[y]-D[x]>=(1<<i):y=Par[y][i]
+    if x==y:return x
+    for i in range(20,-1,-1):
+        if Par[x][i]!=Par[y][i]:
+            x=Par[x][i]
+            y=Par[y][i]
+    return Par[x][0]
 N=int(input())
-A=[[int(x)for x in input().split()]for y in range(N-1)]
-P=list(range(N+1))
-for l in A:union(l[0],l[1])
-print(P)
-print(find(11))
-print(find(6))
+D,M,Par=[-1]*(N+1),[[]for _ in range(N+1)],[[0]*21 for _ in range(N+1)]
+for i in range(N-1):
+    a,b=map(int,input().split())
+    M[a].append(b)
+    M[b].append(a)
+Q=deque([(1,0)])
+while Q:
+    a,c=Q.popleft()
+    D[a]=c
+    for m in M[a]:
+        if D[m]!=-1:continue
+        Par[m][0]=a
+        Q.append((m,c+1))
+for j in range(1,21):
+    for i in range(1,N+1):
+        Par[i][j]=Par[Par[i][j-1]][j-1]
+for p in range(int(input())):
+    a,b=map(int,input().split())
+    print(LCA(a,b))
--- algoritm/20상반기 코딩테스트/영준이의 진짜 BFS/1855.py
+++ algoritm/20상반기 코딩테스트/영준이의 진짜 BFS/1855.py
@@ -3,27 +3,42 @@ sys.stdin = open('1855.txt','r')
 
 from collections import deque
 def LCA(x,y):
-    r=0
     if D[x]>D[y]:x,y=y,x
-    while D[x]!=D[y]:y=T[y];r+=1
-    while x!=y:
-        x=T[x]
-        y=T[y]
-        r+=2
-    return r
+    for i in range(20,-1,-1):
+        if D[y]-D[x]>=1<<i:y=Par[y][i]
+    if x==y:return x
+    for i in range(20,-1,-1):
+        if Par[x][i]!=Par[y][i]:
+            x=Par[x][i]
+            y=Par[y][i]
+    return Par[x][0]
 for tc in range(int(input())):
     N=int(input())
     T=[0,0]+[int(x)for x in input().split()]
-    D=[0,0]*(N-1)
+    D,Par=[-1]*(N+1),[[0]*21 for y in range(N+1)]
     Map=[[]for _ in range(N+1)]
     for t in range(2,N+1):
-        D[t]=D[T[t]]+1
         Map[T[t]].append(t)
+        Map[t].append(T[t])
+    Q=deque([(1,0)])
+    while Q:
+        a,c=Q.popleft()
+        D[a]=c
+        Map[a].sort()
+        for m in Map[a]:
+            if D[m]!=-1:continue
+            Par[m][0]=a
+            Q.append((m,c+1))
+    for j in range(1,21):
+        for i in range(1,N+1):
+            Par[i][j]=Par[Par[i][j-1]][j-1]
     R=0
     Q=deque([1])
-    while Q:
-        a=Q.popleft()
-        Q+=Map[a]
-        if not Q: break
-        R+=LCA(a,Q[0])
+    Check=[0]*(N+1)
+    for t in range(N-1):
+        Check[Q[t]]=1
+        for q in Map[Q[t]]:
+            if not Check[q]:Q.append(q)
+        x=LCA(Q[t],Q[t+1])
+        R+=D[Q[t]]+D[Q[t+1]]-2*D[x]
     print('#%d %d'%(tc+1,R))
