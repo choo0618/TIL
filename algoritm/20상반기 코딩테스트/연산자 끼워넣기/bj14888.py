15120a64c8ac6c731302922f5e5027a31ecfee7c
Hyeon Jin <choo0618@naver.com>
2020-02-21 20:02:02 +0900

    200221 | algorithm

---
+++ algoritm/20상반기 코딩테스트/연산자 끼워넣기/bj14888.py
@@ -0,0 +1,28 @@
+import sys
+sys.stdin = open('bj14888.txt','r')
+
+def Cal(a,b,c):
+    if not c:return a+b
+    elif c==1:return a-b
+    elif c==2:return a*b
+    else:
+        if a<0:return -1*(abs(a)//abs(b))
+        return a//b
+def DFS(r,i):
+    global Min,Max
+    if i==N:
+        Min=min(Min,r)
+        Max=max(Max,r)
+        return
+    for c in range(4):
+        if not L[c] or (c==3 and not Num[i]):continue
+        L[c]-=1
+        DFS(Cal(r,Num[i],c),i+1)
+        L[c]+=1
+N=int(input())
+Num=[int(x)for x in input().split()]
+L=[int(x)for x in input().split()]
+Min,Max=10**10,-10**10
+DFS(Num[0],1)
+print(Max)
+print(Min)
