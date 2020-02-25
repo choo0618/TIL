b2dec4899228766f7eeaf5bea406a1b60eeee35a
Hyeon Jin <choo0618@naver.com>
2020-02-25 21:03:42 +0900

    200225 | algorithm

---
+++ algoritm/20상반기 코딩테스트/내리막 길/bj1520.py
@@ -0,0 +1,20 @@
+import sys
+sys.stdin = open('bj1520.txt','r')
+
+import sys
+sys.setrecursionlimit(10**9)
+def IS(y,x):
+    return -1<y<N and -1<x<M
+def DFS(y,x):
+    if (y,x)==(N-1,M-1):return 1
+    if V[y][x]!=-1:return V[y][x]
+    V[y][x]=0
+    for d in [(0,1),(1,0),(0,-1),(-1,0)]:
+        Y,X=y+d[0],x+d[1]
+        if IS(Y,X) and A[Y][X]<A[y][x]:
+            V[y][x]+=DFS(Y,X)
+    return V[y][x]
+N,M=map(int,input().split())
+A=[[int(x)for x in input().split()]for y in range(N)]
+V=[[-1]*M for _ in range(N)]
+print(DFS(0,0))
