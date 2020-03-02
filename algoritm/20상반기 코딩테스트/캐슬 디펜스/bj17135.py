89c1e4bbbe8f008c59dc59c785d9194f33c05fbd
Hyeon Jin <choo0618@naver.com>
2020-03-03 02:49:17 +0900

    200303 | algorithm

--- algoritm/20상반기 코딩테스트/캐슬 디펜스/bj17135.py
+++ algoritm/20상반기 코딩테스트/캐슬 디펜스/bj17135.py
@@ -2,24 +2,25 @@ import sys
 sys.stdin = open('bj17135.txt','r')
 
 import itertools
-def DFS(y,x,c,m):
-    if y<0 or not -1<x<M:return
-    if A[y][x]:C[m]=1;A[y][x]=0;L.append((y,x));return
-    if c==D:return
-    for Y,X in (y,x-1),(y-1,x),(y,x+1):
-        DFS(Y,X,c+1,m)
-        if C[m]:return
+def IS(y,x):
+    return -1<y<N and -1<x<M
+def BFS(y,x,c):
+    Q=[(y,x,c)]
+    while Q:
+        y,x,c=Q.pop(0)
+        if A[y][x]:Set.add((y,x));return
+        for Y,X in (y,x-1),(y-1,x),(y,x+1):
+            if IS(Y,X) and c:Q.append((Y,X,c-1))
 N,M,D=map(int,input().split())
 A=[[int(x)for x in input().split()]for y in range(N)]
 R=0
-for a,b,c in itertools.combinations(range(M),3):
-    L,now,r=[],N-1,0
-    while now>=0:
-        C=[0,0,0]
-        DFS(now,a,1,0);DFS(now,b,1,1);DFS(now,c,1,2)
-        r+=sum(C)
-        now-=1
+for C in itertools.combinations(range(M),3):
+    L,t=[],N-1
+    while t!=-1:
+        Set=set()
+        for c in C:BFS(t,c,D-1)
+        for i,j in Set:A[i][j]=0;L+=[(i,j)]
+        t-=1
+    R=max(R,len(L))
     for i,j in L:A[i][j]=1
-    R=max(R,r)
 print(R)
-
