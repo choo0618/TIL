90792819590c300e2e10dd35bbbb061643fae79b
Hyeon Jin <choo0618@naver.com>
2020-03-07 23:26:42 +0900

    200307 | algorithm

--- algoritm/20상반기 코딩테스트/구현/구현.py
+++ algoritm/20상반기 코딩테스트/구현/구현.py
@@ -5,13 +5,13 @@ sys.stdin = open('구현.txt','r')
 # N=float(input())
 # print(N*N*math.pi)
 # print(2*N*N)
-#
+
 # print(int(input())-sum(int(input())for _ in range(9)))
-#
+
 # N=int(input())
 # Sum=sum(int(input())for _ in range(N))
 # print("Junhee is cute!" if N//2<Sum else "Junhee is not cute!")
-#
+
 # N,K=map(int,input().split())
 # Map=[0]*(N+1)
 # r=0
@@ -25,7 +25,7 @@ sys.stdin = open('구현.txt','r')
 #             r=t
 #         t+=i
 # print(r)
-#
+
 # while True:
 #     L=[int(x)for x in input().split()]
 #     if not sum(L):break
@@ -111,10 +111,92 @@ sys.stdin = open('구현.txt','r')
 # for l in L:Dic[abs(100-l)].append(l);Min=min(Min,abs(100-l))
 # print(max(Dic[Min]))
 
-R,S=0,0
-for i in range(10):
-    S+=int(input())
-    if abs(R-100)>=abs(S-100):R=S
-print(R)
+# R,S=0,0
+# for i in range(10):
+#     S+=int(input())
+#     if abs(R-100)>=abs(S-100):R=S
+# print(R)
+
+# for t in range(int(input())):
+#     A,B,C,D,a,b,c,d=map(int,input().split())
+#     print(max(1,A+a)+5*max(1,B+b)+2*max(0,C+c)+2*(D+d))
+
+# N,W,H=map(int,input().split())
+# M=(W**2+H**2)**0.5
+# for _ in range(N):print('DA' if int(input())<=M else 'NE')
+
+# W=[int(input())for x in range(10)]
+# K=[int(input())for x in range(10)]
+# W.sort()
+# K.sort()
+# print(sum(W[7:]),sum(K[7:]))
+
+# e,f,c=map(int,input().split())
+# R,S=0,e+f
+# while S>=c:
+#     R+=S//c
+#     S=sum(divmod(S,c))
+# print(R)
+
+# N=int(input())
+# L=[int(x)for x in input().split()]
+# Map=[0]*(N+1)
+# fact=[1]*N
+# for i in range(1,N):
+#     fact[i]=fact[i-1]*i
+# if L[0]==1:
+#     for i in range(1,N+1):
+#         cnt=1
+#         for j in range(1,N+1):
+#             if Map[j]:continue
+#             if cnt*fact[N-i]>=L[1]:
+#                 Map[j]=1
+#                 print(j,end=' ')
+#                 L[1]-=(cnt-1)*fact[N-i]
+#                 break
+#             cnt+=1
+# else:
+#     K=1
+#     for i in range(1,N+1):
+#         cnt=1
+#         for j in range(1,N+1):
+#             if Map[j]:continue
+#             if L[i]==j:
+#                 Map[j]=1
+#                 K+=(cnt-1)*fact[N-i]
+#                 break
+#             cnt+=1
+#     print(K)
+
+# from math import factorial
+# def Q1(k):
+#     if len(l)==0:return []
+#     f=factorial(len(l)-1)
+#     i=0
+#     while f<=k:k-=f;i+=1
+#     c=l.pop(i)
+#     return [c]+Q1(k)
+# def Q2():
+#     k=0
+#     for a in L:
+#         idx=l.index(a)
+#         k+=idx*factorial(len(l)-1)
+#         l.pop(idx)
+#     return k+1
+# N=int(input())
+# q,*L=list(map(int,input().split()))
+# l=[i+1 for i in range(N)]
+# if q==1:
+#     k=L[0]
+#     print(' '.join(map(str,Q1(k-1))))
+# else:print(Q2())
+
+N=int(input())
+r=0
+for i in range(1,N+1):
+    for j in range(i,N+1):
+        if i*j>N:break
+        r+=1
+print(r)
 
 
