707171ea07ee4a7c27a1b6c789dc364595bdd15a
Hyeon Jin <choo0618@naver.com>
2020-03-19 18:01:33 +0900

    200319 | algorithm

--- algoritm/20상반기 코딩테스트/A/A.py
+++ algoritm/20상반기 코딩테스트/A/A.py
@@ -76,8 +76,78 @@ sys.stdin = open('A.txt','r')
 #     R+=1
 # print(R)
 
-A,B,C=map(int,input().split())
-if B>=C:print(-1)
-else:print(A//(C-B)+1)
+# A,B,C=map(int,input().split())
+# if B>=C:print(-1)
+# else:print(A//(C-B)+1)
 
+# for t in range(int(input())):
+#     N=int(input())
+#     K=int(input())
+#     DP=[list(range(K+1))]+[[0]*(K+1) for _ in range(N)]
+#     for n in range(1,N+1):
+#         for k in range(1,K+1):
+#             DP[n][k]=DP[n-1][k]+DP[n][k-1]
+#     print(DP[N][K])
+
+# X=0
+# Y=0
+# for _ in range(3):
+#     x,y=map(int,input().split())
+#     X^=x
+#     Y^=y
+# print(X,Y)
+
+# for t in range(int(input())):
+#     x1,y1,r1,x2,y2,r2=map(int,input().split())
+#     d=(abs(x1-x2)**2+abs(y1-y2)**2)**0.5
+#     if (x1,y1)==(x2,y2):
+#         if r1==r2:print(-1)
+#         else:print(0)
+#     elif abs(r1-r2)<d and r1+r2>d:print(2)
+#     elif abs(r1-r2)==d or r1+r2==d:print(1)
+#     else:print(0)
+
+# Map=[1]*1001
+# Map[0]=Map[1]=0
+# for i in range(2,500):
+#     if not Map[i]:continue
+#     Map[i]=1
+#     for n in range(2*i,1001,i):Map[n]=0
+# N,R=int(input()),0
+# for n in map(int,input().split()):R+=Map[n]
+# print(R)
+
+# N=int(input())
+# M=int(input())
+# Map=[1]*(M+1)
+# Map[0]=Map[1]=0
+# for i in range(2,(M+1)//2):
+#     if not Map[i]:continue
+#     Map[i]=1
+#     for n in range(2*i,M+1,i):Map[n]=0
+# R,r=0,0
+# for n in range(M,N-1,-1):
+#     if Map[n]:R+=n;r=n
+# if R:print(R);print(r)
+# else:print(-1)
+
+# Map=[1]*10001
+# Map[0]=Map[1]=0
+# for i in range(2,5000):
+#     if not Map[i]:continue
+#     Map[i]=1
+#     for n in range(2*i,10001,i):Map[n]=0
+# for t in range(int(input())):
+#     N=int(input())
+#     for i in range(N//2,1,-1):
+#         if Map[i] and Map[N-i]:print(i,N-i);break
+
+
+N=int(input())
+n=665
+while N:
+    n+=1
+    tmp=str(n)
+    if '666' in tmp:N-=1
+print(n)
 
