0604eaf8fe673e0f61ea23e97a7cfddb0c523d5d
Hyeon Jin <choo0618@naver.com>
2020-04-27 21:41:17 +0900

    200427 | algorithm

--- algoritm/20상반기 코딩테스트/과외맨/bj5213.py
+++ algoritm/20상반기 코딩테스트/과외맨/bj5213.py
@@ -3,15 +3,18 @@ sys.stdin = open('bj5213.txt','r')
 
 from collections import deque
 def IS(y,x):
-    return -1<y<N and -1<x<2*N
+    return -1<y<N and -1<x<2*N and nMap[y][x]!=-1
+def Find(y,x):
+    for Y,X in (y,x+1),(y,x-1):
+        if tMap[Y][X]==tMap[y][x]:return Y,X
 def Check(i):
     L=[]
     while i!=R[i]:
         L.append(i)
         i=R[i]
-    return L
+    return L[::-1]
 N=int(input())
-nMap,tMap,t=[],[],1
+nMap,tMap,T,I=[],[],1,0
 for i in range(N):
     nl,tl=[],[]
     Len=N
@@ -19,29 +22,26 @@ for i in range(N):
     for j in range(Len):
         x,y=map(int,input().split())
         nl.append(x);nl.append(y)
-        tl.append(t);tl.append(t)
-        t+=1
+        tl.append(T);tl.append(T)
+        T+=1
     if i%2:nl.append(-1);tl.append(-1)
     nMap.append(nl);tMap.append(tl)
-R=[int(x)for x in range(t)]
+R=[int(x)for x in range(T)]
 V=[[0]*(2*N)for _ in range(N)]
-R[1],V[0][0]=0,1
-Q=deque([(0,0)])
+R[1]=0
+Q=deque([(0,1)])
+V[0][0]=V[0][1]=1
 while Q:
     y,x=Q.popleft()
     for Y,X in (y,x+1),(y+1,x),(y,x-1),(y-1,x):
-        if IS(Y,X) and nMap[Y][X]!=-1 and not V[Y][X]:
-            n,nn,t,nt=nMap[y][x],nMap[Y][X],tMap[y][x],tMap[Y][X]
-            if t!=nt:
-                if n!=nn:continue
-                else:R[nt]=t
-            V[Y][X]=1
-            Q.append((Y,X))
-for r in range(t,0,-1):
-    if R[r]!=r:
-        L=Check(r)
-        if L[-1]!=1:continue
-        L.reverse()
-        print(len(L))
-        print(*L)
-        break
+        if not IS(Y,X) or V[Y][X]:continue
+        t,nt=tMap[y][x],tMap[Y][X]
+        if t!=nt and nMap[y][x]!=nMap[Y][X]:continue
+        if nt!=t:R[nt]=t
+        I=max(I,nt)
+        i,j=Find(Y,X)
+        V[Y][X]=V[i][j]=1
+        Q+=[(Y,X),(i,j)]
+L=Check(I)
+print(len(L))
+print(*L)
