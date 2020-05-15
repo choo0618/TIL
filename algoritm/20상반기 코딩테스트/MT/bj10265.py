32098f5713d587dc6a2e2c3e7e8bf6f0bf9d4222
Hyeon Jin <choo0618@naver.com>
2020-05-15 19:03:13 +0900

    200515 | algorithm

--- algoritm/20상반기 코딩테스트/MT/bj10265.py
+++ algoritm/20상반기 코딩테스트/MT/bj10265.py
@@ -3,9 +3,9 @@ sys.stdin = open('bj10265.txt','r')
 
 def DFS(x,d):
     global r,c
-    if DP[x]:
-        if C[x]:r[C[x]-1][1]+=d
-        return DP[x],C[x]
+    if C[x]:
+        r[C[x]-1][1]+=d
+        return C[x]
     if V[x]:
         cnt,tmp,C[x]=1,L[x],c
         while x!=tmp:
@@ -14,35 +14,31 @@ def DFS(x,d):
             tmp=L[tmp]
         r.append([cnt,d-cnt])
         c+=1
-        return cnt,c-1
+        return c-1
     V[x]=1
-    tmp,tmp1=DFS(L[x],d+1)
-    if not C[x]:tmp+=1
-    DP[x]+=tmp
-    C[x]=tmp1
-    return DP[x],tmp1
+    C[x]=DFS(L[x],d+1)
+    return C[x]
 N,K=map(int,input().split())
 L=[0]+[int(x)for x in input().split()]
 c=1
 C=[0]*(N+1)
 V=[0]*(N+1)
-DP=[0]*(N+1)
 r=[]
 for i in range(1,N+1):
-    if not DP[i]:DFS(i,0)
+    if not V[i]:DFS(i,0)
 R,tmp=[0],0
 for n,p in r:
     if n<=K:R.append((n,p));tmp+=1
-KS=[[-1]*(K+1) for _ in range(tmp+1)]
+DP=[[-1]*(K+1) for _ in range(tmp+1)]
 for i in range(1,tmp+1):
     w,v=R[i]
-    KS[i][w]=v
+    DP[i][w]=v
     for j in range(1,K+1):
         if j>w:
-            if KS[i-1][j-w]!=-1:KS[i][j]=max(KS[i-1][j-w]+v,KS[i-1][j])
-            else:KS[i][j]=max(KS[i][j],KS[i-1][j])
-        else:KS[i][j]=max(KS[i][j],KS[i-1][j])
+            if DP[i-1][j-w]!=-1:DP[i][j]=max(DP[i-1][j-w]+v,DP[i-1][j])
+            else:DP[i][j]=max(DP[i][j],DP[i-1][j])
+        else:DP[i][j]=max(DP[i][j],DP[i-1][j])
 ans=0
 for i in range(1,K+1):
-    if KS[tmp][i]!=-1:ans=max(ans,i+KS[tmp][i])
+    if DP[tmp][i]!=-1:ans=max(ans,i+DP[tmp][i])
 print(min(ans,K))
