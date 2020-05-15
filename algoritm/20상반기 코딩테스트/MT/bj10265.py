02cdad05b05d16fe25f84ab42ccc0b8700782328
Hyeon Jin <choo0618@naver.com>
2020-05-15 18:49:50 +0900

    200515 | algorithm

--- algoritm/20상반기 코딩테스트/MT/bj10265.py
+++ algoritm/20상반기 코딩테스트/MT/bj10265.py
@@ -1,23 +1,26 @@
 import sys
 sys.stdin = open('bj10265.txt','r')
 
-def DFS(x):
-    global c,r
-    if DP[x]:return DP[x]
+def DFS(x,d):
+    global r,c
+    if DP[x]:
+        if C[x]:r[C[x]-1][1]+=d
+        return DP[x],C[x]
     if V[x]:
-        cnt,tmp,C[x]=1,L[x],1
+        cnt,tmp,C[x]=1,L[x],c
         while x!=tmp:
             cnt+=1
-            C[tmp]=1
+            C[tmp]=c
             tmp=L[tmp]
-        if cnt<=K:r.append(cnt)
-        return cnt
+        r.append([cnt,d-cnt])
+        c+=1
+        return cnt,c-1
     V[x]=1
-    tmp=DFS(L[x])
+    tmp,tmp1=DFS(L[x],d+1)
     if not C[x]:tmp+=1
     DP[x]+=tmp
-    # if DP[x]<=K:r.append(DP[x])
-    return DP[x]
+    C[x]=tmp1
+    return DP[x],tmp1
 N,K=map(int,input().split())
 L=[0]+[int(x)for x in input().split()]
 c=1
@@ -26,14 +29,20 @@ V=[0]*(N+1)
 DP=[0]*(N+1)
 r=[]
 for i in range(1,N+1):
-    if not DP[i]:DFS(i)
-    if C[i]==0 and DP[i]<=K:r.append(DP[i])
-print(DP)
-r.sort()
-print(r)
-R=sum(r)
-if R>K:
-    for n in r:
-        R-=n
-        if R<=K:break
-print(R)
+    if not DP[i]:DFS(i,0)
+R,tmp=[0],0
+for n,p in r:
+    if n<=K:R.append((n,p));tmp+=1
+KS=[[-1]*(K+1) for _ in range(tmp+1)]
+for i in range(1,tmp+1):
+    w,v=R[i]
+    KS[i][w]=v
+    for j in range(1,K+1):
+        if j>w:
+            if KS[i-1][j-w]!=-1:KS[i][j]=max(KS[i-1][j-w]+v,KS[i-1][j])
+            else:KS[i][j]=max(KS[i][j],KS[i-1][j])
+        else:KS[i][j]=max(KS[i][j],KS[i-1][j])
+ans=0
+for i in range(1,K+1):
+    if KS[tmp][i]!=-1:ans=max(ans,i+KS[tmp][i])
+print(min(ans,K))
