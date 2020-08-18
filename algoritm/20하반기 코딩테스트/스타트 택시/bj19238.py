80108ff2204d0f414525122d6251e6e006ed1e2f
Hyeon Jin <choo0618@naver.com>
2020-08-18 17:41:00 +0900

    200818 | algorithm

---
+++ algoritm/20하반기 코딩테스트/스타트 택시/bj19238.py
@@ -0,0 +1,44 @@
+import sys
+sys.stdin = open('bj19238.txt','r')
+
+from collections import deque
+def IS(y,x):
+    return -1<y<N and -1<x<N
+def Sol(y,x,g,c):
+    if c==0:n,Guest[y][x]=Guest[y][x],0
+    V=[[0]*N for _ in range(N)]
+    V[y][x]=1
+    Q=deque([(y,x,0)])
+    tmp,L=10**9,[]
+    while Q:
+        y,x,d=Q.popleft()
+        if c and Guest[y][x]:tmp=d;L.append((y,x,Guest[y][x]));continue
+        if c and d>tmp:break
+        if c==0 and n in State[y][x]:tmp=d;break
+        for Y,X in (y-1,x),(y,x-1),(y,x+1),(y+1,x):
+            if not IS(Y,X) or Map[Y][X] or V[Y][X]:continue
+            V[Y][X]=1
+            Q.append((Y,X,d+1))
+    g-=tmp
+    if g<0:return -1,-1,-1
+    if not c:return y,x,g+2*tmp
+    L.sort()
+    return L[0][0],L[0][1],g
+N,M,G=map(int,input().split())
+Map=[[int(x)for x in input().split()]for y in range(N)]
+Guest=[[0]*N for _ in range(N)]
+State=[[[] for _ in range(N)] for _ in range(N)]
+Y,X=map(int,input().split())
+Y-=1;X-=1
+for m in range(M):
+    i,j,I,J=map(int,input().split())
+    i-=1;j-=1;I-=1;J-=1
+    Guest[i][j]=m+1
+    State[I][J].append(m+1)
+while M:
+    Y,X,G=Sol(Y,X,G,1)
+    if G<0:break
+    Y,X,G=Sol(Y,X,G,0)
+    if G<0:break
+    M-=1
+print(G)
